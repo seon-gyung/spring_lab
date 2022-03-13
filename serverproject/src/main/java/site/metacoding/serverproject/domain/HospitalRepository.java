468926dc9dad88802d31369c77ec8ae0057c79e0
seon-gyung <97711596+seon-gyung@users.noreply.github.com>
2022-03-13 16:19:23 +0900

    Delete serverproject directory

--- serverproject/src/main/java/site/metacoding/serverproject/domain/HospitalRepository.java
+++
@@ -1,9 +0,0 @@
-package site.metacoding.serverproject.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository // 생략 가능
-public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
-
-}
